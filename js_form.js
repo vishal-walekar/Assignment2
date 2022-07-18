7a3fa2d46efe7b1bc53307d892b36ce3d95deadd
Vishal Walekar <vishal.walekar@olacabs.com>
2022-07-18 15:12:14 +0530

    Edit Delete Operations

--- js_form.js
+++ js_form.js
@@ -1,14 +1,18 @@
 var selectedRow = null;
-function onFormSubmit(e){
+function onFormSubmit(){
     event.preventDefault();
-    var formData =readFormData();
-    if(selectedRow === null){
+
+    var formData = readFormData();
+    if(selectedRow == null)
+    {
         insertNewRecord(formData);
     }
-    else{
+    else
+    {
         updateRecord(formData);
     }
-    resetForm();
+    // insertNewRecord(formData);
+    resetForm(formData);
 }
 
 //retrive the data 
@@ -25,25 +29,26 @@ function readFormData(){
 function insertNewRecord(data){
     var table=document.getElementById("storeList").getElementsByTagName('tbody')[0];
     var newRow=table.insertRow(table.length);
-    var cell1=newRow.insertCell(0);
+
+    cell1=newRow.insertCell(0);
         cell1.innerHTML=data.FirstName;
-    var cell2=newRow.insertCell(1);
+    cell2=newRow.insertCell(1);
         cell2.innerHTML=data.LastName;  
-    var cell3=newRow.insertCell(2);
+    cell3=newRow.insertCell(2);
         cell3.innerHTML=data.Email;
-    var cell4=newRow.insertCell(3);
+    cell4=newRow.insertCell(3);
         cell4.innerHTML=data.Contact;  
-    //var cell5=newRow.insertCell(4);
-       //cell5.innerHTML = `<button onClick=`onEdit(this)`>Edit</button>  <button onClick=`onDelete(this)`>Delete</button>`
+    cell5=newRow.insertCell(4);
+       cell5.innerHTML ="<button onClick='onEdit(this)'>Edit</button>   <button onClick='onDelete(this)'>Delete</button>";
 }
 
 // Edit the data
 function onEdit(td){
     selectedRow = td.parentElement.parentElement;
-    document.getElementById(`FristName`).value=selectedRow.cells[0].innerHTML;
-    document.getElementById(`LastName`).value=selectedRow.cells[1].innerHTML;
-    document.getElementById(`Email`).value=selectedRow.cells[2].innerHTML;
-    document.getElementById(`Contact`).value=selectedRow.cells[3].innerHTML;
+    document.getElementById("FirstName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("LastName").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("Email").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("Contact").value = selectedRow.cells[3].innerHTML;
 }
 
 function updateRecord(formData){
@@ -58,15 +63,16 @@ function onDelete(td){
     if(confirm(`Do you want to delete this record`)){
         row=td.parentElement.parentElement;
         document.getElementById(`storeList`).deleteRow(row.rowIndex);
+        resetForm();
     }
-    resetForm();
+    
 }
 
 //Reset function
 function resetForm(){
-    document.getElementById(`FirstName`).value= ``;
-    document.getElementById(`LastName`).value= ``;
-    document.getElementById(`Email`).value= ``;
-    document.getElementById(`Contact`).value= ``;
+    document.getElementById(`FirstName`).value= "";
+    document.getElementById(`LastName`).value= "";
+    document.getElementById(`Email`).value= "";
+    document.getElementById(`Contact`).value= "";
 
 }
